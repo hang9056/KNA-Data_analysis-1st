79cb3fddb2273dfe50ae346a51e71fd8f5e16438
hang9056 <hang90563333@gmail.com>
2026-07-15 13:55:06 +0900

    variable 학습

---
+++ 04_variable.py
@@ -0,0 +1,42 @@
+temp = 25
+print(temp)
+name = "센서A"
+print(name)
+
+temp = 30  # temp 30으로 수정
+print(temp)
+temperature = temp
+print(temperature)
+
+my_number = 17
+print(my_number)
+mood = "행복"
+print(mood)
+
+age = 28  # 나이
+label = "나이"
+print(label)
+print(age)
+
+x = 10
+print(x)  # 10
+x = x + 5
+print(x)  # 15
+x = x * 2
+print(x)  # 30
+
+a = 100
+b = a
+a = 999
+print(a)  # 999
+print(b)  # 100
+
+temp = 25
+print(temp)  # 25
+score = 90
+print(score)  # 90
+
+temp = 25  # 온도(섭씨)
+count = 3  # 센서 개수
+# temp = 100 # 실행 안 함
+print(temp)  # 25
