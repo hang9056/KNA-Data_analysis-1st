98c836f955d30d44aa8eb720ccd89197c5e45027
hang9056 <hang90563333@gmail.com>
2026-07-16 11:04:16 +0900

    연산자사용완료

--- 06_operator_update.py
+++ 06_operator_update.py
@@ -1,5 +1,30 @@
-# dfaskfsdk
-# sdaaffsadfa
-sdfadfafdsf
-dsfafdfssdfasfasdfas
-sdafsdd
+print(5 == 5)
+print(5 != 7)
+print(5 > 7)
+print(5 < 7)
+print(11 >= 7)
+print(7 <= 5)
+print("========================")
+
+temp = 85
+print(temp > 80 and temp < 90)
+pressure = 5
+print(pressure >= 3 or pressure <= 7)
+
+print("========================")
+stock = 100
+stock += 50
+stock -= 30
+stock += 5
+print(stock)
+
+
+불량률 = (23 / 500) * 100
+print("불량률: ", 불량률, "%")
+가동률 = (21 / 24) * 100
+print("가동률: ", 가동률, "%")
+
+print("========================")
+minutes = 500
+print(minutes // 60)  # 8 (시간)
+print(minutes % 60)  # 20 (분)
