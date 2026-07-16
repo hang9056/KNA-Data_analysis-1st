94aadf3ef323a8290dba4bbcfc61ebd7d19e9a69
hang9056 <hang90563333@gmail.com>
2026-07-16 17:51:12 +0900

    0716

--- 07_input.py
+++ 07_input.py
@@ -1,40 +1,66 @@
-# age = int(input("태어난 해: "))
-# print("올해 나이 : ", 2026 - age, "세")
+# # age = int(input("태어난 해: "))
+# # print("올해 나이 : ", 2026 - age, "세")
 
-# city = input("사는 곳: ")
-# country = input("사는 나라: ")
-# print("사는 곳: ", city, country)
+# # city = input("사는 곳: ")
+# # country = input("사는 나라: ")
+# # print("사는 곳: ", city, country)
 
 
-# a = int(input("첫 번째 숫자: "))
-# b = int(input("두 번째 숫자: "))
-# print("두 숫자의 합: ", a + b)
-# print("두 숫자의 차: ", a - b)
+# # a = int(input("첫 번째 숫자: "))
+# # b = int(input("두 번째 숫자: "))
+# # print("두 숫자의 합: ", a + b)
+# # print("두 숫자의 차: ", a - b)
 
-# temp = int(input("온도: "))
-# print("온도가 80보다 큰가? ", temp > 80)
-# print("온도가 0보다 크거나 같은가? ", temp >= 0)
+# # temp = int(input("온도: "))
+# # print("온도가 80보다 큰가? ", temp > 80)
+# # print("온도가 0보다 크거나 같은가? ", temp >= 0)
 
-# a = int(input("첫번째 점수: "))
-# b = int(input("두번째 점수: "))
-# c = int(input("세번째 점수: "))
-# total = a + b + c
-# ave = total / 3
-# print("총점: ", total)
-# print("평균: ", ave)
+# # a = int(input("첫번째 점수: "))
+# # b = int(input("두번째 점수: "))
+# # c = int(input("세번째 점수: "))
+# # total = a + b + c
+# # ave = total / 3
+# # print("총점: ", total)
+# # print("평균: ", ave)
 
-code = "PUMP_A"
-state = "정상"
-hours = 1200
-day = "2025-01-15"
-card = (
-    "설비: "
-    + code
-    + "\n상태: "
-    + state
-    + "\n가동: "
-    + str(hours)
-    + "시간\n점검: "
-    + day
-)
-print(card)
+# code = "PUMP_A"
+# state = "정상"
+# hours = 1200
+# day = "2025-01-15"
+# card = (
+#     "설비: "
+#     + code
+#     + "\n상태: "
+#     + state
+#     + "\n가동: "
+#     + str(hours)
+#     + "시간\n점검: "
+#     + day
+# )
+# print(card)
+
+# abc = "abcdefghijklmnopqrstuvwxyz"
+# print(abc[18] + abc[0] + abc[13] + abc[6] + abc[24] + abc[4] + abc[14] + abc[15])
+
+# word = "temp_sensor"
+# print(word[:4])
+
+
+# word = "temp_sensor"
+# print(word[5:])
+
+word = "sensor_01"
+print(word[-2:])
+
+word = "PYTHON"
+print(word[::2])
+
+word = "PYTHON"
+print(word[::-1])
+
+number = "12345678911"
+print(len(number))  # Get the length of the string directly.
+
+
+text = "banana"
+print(text.count("a"))
