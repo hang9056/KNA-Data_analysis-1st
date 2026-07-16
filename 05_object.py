d5417e93d621cad68a80456cc801b34c1e8674f3
hang9056 <hang90563333@gmail.com>
2026-07-16 09:34:45 +0900

    Add object and operator update practice files

---
+++ 05_object.py
@@ -0,0 +1,81 @@
+emoji = "❤️"
+# bool - 분리언형
+ok = True
+no = False
+
+# 비교할 경우 bool 로 출려
+print(100 < 5)
+
+count = 3  # int
+temp = 25.5  # float
+name = "센서A"  # str
+is_ok = True  # bool
+print(count, temp, name, is_ok)  # 3 25.5 센서A True
+
+
+print(type(count))  # <class 'int'>
+print(type(temp))  # <class 'float'>
+print(type(name))  # <class 'str'>
+print(type(is_ok))  # <class 'bool'>
+
+
+print(type(100))  # <class 'int'>
+print(type(100.0))  # <class 'float'>
+print(type("100"))  # <class 'str'>
+
+
+print(3 + 5)  # 8
+print("3" + "5")  # 35
+print("10" + "20")  # 1020
+
+
+print(3 > 2)  # True
+print(5 == 5)  # True
+print(type(3 > 2))  # <class 'bool'>
+
+count = 3
+print(count, type(count))
+
+
+count = 3
+print(count, type(count))
+count = 3.0
+print(count, type(count))
+count = "3"
+print(count, type(count))
+
+
+device_temp = 25.5  # float
+check_count = 12  # int
+device_name = "펌프A"  # str
+is_normal = True  # bool
+
+print(is_normal, type(is_normal))  # True <class 'bool'>
+
+
+# 산술연산자
+
+print("안녕", "하세요")
+print("안녕" + "하세요")
+
+a = 17
+b = 5
+
+print(a + b)  # 22
+print(a - b)  # 12
+print(a * b)  # 85
+print(a / b)  # 3.4 (나눗셈은 실수)
+print(a // b)  # 3 (몫)
+print(a % b)  # 2 (나머지)
+print(a**b)  # 1419857 (거듭제곱)
+
+
+x = 80
+y = 91
+z = 90
+side = 7
+print((x + y + z) / 3)  # 87.0 (평균)
+print(side**2)  # 49 (정사각형 넓이)
+print(3 * 4 * 5)  # 60 (직육면체 부피)
+
+
---
+++ 06_operator_update.py
@@ -0,0 +1,2 @@
+# dfaskfsdk
+# sdaaffsadfa
